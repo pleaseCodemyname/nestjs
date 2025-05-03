f9e30cf1c65ec21b0b1b1192d074a0200b122553
Sanghyun Yun <545austin@naver.com>
2025-05-03 18:37:35 +0900

    nestJS_Post&Get&Put&Delete

--- cf_sns/src/posts/posts.controller.ts
+++ cf_sns/src/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 
 /**
@@ -10,7 +19,8 @@ import { PostsService } from './posts.service';
  */
 
 // Post 인터페이스 생성
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
@@ -18,20 +28,119 @@ interface Post {
   commentCount: number;
 }
 
+// PostModel을 List로[]
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스 민지',
+    content: '화장 고치고 있는 민지',
+    likeCount: 1000000,
+    commentCount: 777777,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스 해린',
+    content: '노래 연습하고 있는 해린',
+    likeCount: 1000000,
+    commentCount: 777777,
+  },
+  {
+    id: 3,
+    author: 'blackpink_official',
+    title: '블랙핑크 로제',
+    content: '종합운동장에서 공영중인 로제',
+    likeCount: 1000000,
+    commentCount: 777777,
+  },
+];
+
 // 컨트롤러 첫번쨰 파라미터에는 "AppController"이라는 클래스 안에 있는 모든 엔드포인트들의 접두어를 붙이는 역할, Prefix역할
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  // Json 반환, @Get(데코레이터)에도 엔드포인트 명시 가능
+  // 1) GET /posts
+  //    모든 포스트를 다 가져온다.
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스 민지',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 1000000,
-      commentCount: 7777777,
+  getPosts() {
+    return posts;
+  }
+
+  // 2) GET /posts/:id
+  //    id에 해당되는 post를 가져온다.
+  //    ex) id=1일 경우, id가 1인 포스트를 가져온다.
+  //    parameter를 url로부터 가져올껀데, 가져오려는 parameter는 id이다. 가져온 파라미터는 id라는 변수에 저장을하고, id type은 string이다.
+  @Get(':id')
+  getPost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id); // post에 있응 id: number인데, 여기의 id: string이라서 +를 붙여주면 해결
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+    return post;
+  }
+
+  // 3) POST /posts
+  //    POST를 생성한다.
+  @Post()
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string,
+  ) {
+    const post = {
+      id: posts[posts.length - 1].id + 1,
+      author, // author : author
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0,
     };
+
+    posts = [...posts, post];
+    return post;
+  }
+
+  // 4) PUT /posts/:id
+  //    id에 해당되는 POST를 변경한다.
+  @Put(':id')
+  putPost(
+    @Param('id') id: string,
+    @Body('author') author?: string,
+    @Body('title') title?: string,
+    @Body('content') content?: string,
+  ) {
+    const post = posts.find((post) => post.id === +id);
+
+    if (!post) {
+      throw new NotFoundException();
+    }
+    if (author) {
+      post.author = author;
+    }
+    if (title) {
+      post.title = title;
+    }
+    if (content) {
+      post.content = content;
+    }
+    posts = posts.map((prevPost) => (prevPost.id === +id ? post : prevPost));
+
+    return post;
+  }
+
+  // 5) DELETE /posts/:id
+  //    id에 해당되는 POST를 삭제한다.
+  @Delete(':id')
+  deletePost(@Param('id') id: string) {
+    const post = posts.find((post) => post.id === +id);
+    if (!post) {
+      throw new NotFoundException();
+    }
+    posts = posts.filter((post) => post.id !== +id);
+
+    return id;
   }
 }
