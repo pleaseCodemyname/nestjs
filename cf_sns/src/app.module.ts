12bcd5717aa7a7bc47df9792c105f1dc17a3cf3e
Sanghyun Yun <545austin@naver.com>
2025-05-19 00:19:52 +0900

    nestJS_Static_File_Serving

--- cf_sns/src/app.module.ts
+++ cf_sns/src/app.module.ts
@@ -17,11 +17,20 @@ import {
   ENV_DB_PORT_KEY,
   ENV_DB_USERNAME_KEY
 } from './common/const/env-keys.const';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PUBLIC_FOLDER_PATH } from './common/const/path.const';
 
 @Module({
   // 다른 모듈을 불러올 때 사용
   imports: [
     PostsModule,
+    ServeStaticModule.forRoot({
+      rootPath: PUBLIC_FOLDER_PATH,
+      serveRoot: '/public',
+      serveStaticOptions: {
+        index: false
+      }
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true
